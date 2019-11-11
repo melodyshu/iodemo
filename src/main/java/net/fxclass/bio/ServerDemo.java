2a482c7a7ac299a9613e0705a880713df8c51a14
melodyshu <1321497435@qq.com>
2019-11-11 14:35:53 +0800

    bio

---
+++ src/main/java/net/fxclass/bio/ServerDemo.java
@@ -0,0 +1,20 @@
+package net.fxclass.bio;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerDemo {
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket=new ServerSocket(9090);
+        //获取一个客户端socket
+        Socket client= serverSocket.accept();//阻塞
+        System.out.println("客户端:"+client.getInetAddress().getLocalHost()+"已连接到服务器");
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        String msg=br.readLine();
+        System.out.println("客户端："+msg);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        bw.write(msg+"\n");
+        bw.flush();
+    }
+}
